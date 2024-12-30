e0b0d4b17d15ba94f34b55a1e661157512d401eb
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-12-29 22:21:29 -0600

    feat: solve all test cases on coin change

--- src/coin_change.rs
+++ src/coin_change.rs
@@ -14,41 +14,20 @@ use rstest::rstest;
 #[case((4, vec![1,2,3]), 4)]
 #[case((10, vec![2,5,3,6]), 5)]
 #[case((166, vec![5,37,8,39,33,17,22,32,13,7,10,35,40,2,43,49,46,19,41,1,12,11,28]), 96_190_959)]
-fn test_get_ways(#[case] input: (isize, Vec<isize>), #[case] expected: isize) {
+fn test_get_ways(#[case] input: (usize, Vec<usize>), #[case] expected: usize) {
     let (n, c) = input;
     assert_eq!(getWays(n, &c), expected);
 }
 
-fn getWays(n: isize, c: &[isize]) -> isize {
-    // sort currencies from smallest to largest
-    let mut s = vec![0; c.len()];
-    s.clone_from_slice(c);
-    s.sort();
-    // then recur over denoms to generate ways
-    let ways = change(n.try_into().unwrap(), &s);
-    println!("ways: {ways:#?}");
-    // then count the ways & return
-    ways.len().try_into().unwrap()
-}
+fn getWays(n: usize, c: &[usize]) -> usize {
+    let mut ways = vec![0; n + 1];
+    ways[0] = 1;
 
-fn change(amount: isize, denoms: &[isize]) -> Vec<Vec<isize>> {
-    if amount == 0 {
-        println!("change({amount}, {denoms:?}) --> [[],]");
-        return vec![vec![]];
-    } else if amount < 0 || denoms.len() == 0 {
-        println!("change({amount}, {denoms:?}) --> []");
-        return vec![];
-    } else {
-        let mut r = vec![];
-
-        for mut way in change(amount - denoms[0], denoms) {
-            way.insert(0, denoms[0]);
-            r.push(way);
+    for coin in c {
+        for j in *coin..(n + 1) {
+            ways[j] = ways[j] + ways[j - coin];
         }
-
-        r.append(&mut change(amount, &denoms[1..]));
-        println!("change({amount}, {denoms:?}) --> {r:?}");
-
-        r
     }
+
+    ways[n]
 }
