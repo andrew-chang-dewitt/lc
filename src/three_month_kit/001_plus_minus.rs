941d536e8f8399798541485953f67c467956e29c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2025-01-15 13:32:18 -0600

    feat(001-3mk): solve using rust_decimal

--- src/three_month_kit/001_plus_minus.rs
+++ src/three_month_kit/001_plus_minus.rs
@@ -71,6 +71,8 @@
 //! There are 3 positive numbers, 2 negative numbers, and 1 zero in the array.
 //! The proportions of occurrence are positive: `3/6 = 0.500000`, negative: `2/6 = 0.333333` and zeros: `3/6 = 0.166667`.
 
+use std::cmp::Ordering;
+
 use rstest::rstest;
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
@@ -89,11 +91,61 @@ fn plusMinus(arr: &[i32]) {
 }
 
 fn get_ratios(arr: &[i32]) -> (Decimal, Decimal, Decimal) {
-    (dec!(1.0), dec!(1.0), dec!(1.0))
+    let (pos, neg, zer) = count_pnz(arr);
+    let len = arr.len();
+
+    let p = pos as f32 / len as f32;
+    println!("pos {pos} / {len} => {p}");
+    let n = neg as f32 / len as f32;
+    println!("neg {neg} / {len} => {n}");
+    let z = zer as f32 / len as f32;
+    println!("zer {zer} / {len} => {z}");
+
+    (
+        Decimal::try_from(p)
+            .unwrap()
+            .round_dp_with_strategy(6, rust_decimal::RoundingStrategy::MidpointAwayFromZero),
+        Decimal::try_from(n)
+            .unwrap()
+            .round_dp_with_strategy(6, rust_decimal::RoundingStrategy::MidpointAwayFromZero),
+        Decimal::try_from(z)
+            .unwrap()
+            .round_dp_with_strategy(6, rust_decimal::RoundingStrategy::MidpointAwayFromZero),
+    )
 }
 
 #[rstest]
 #[case(vec![-4, 3, -9, 0, 4, 1], (dec!(0.500000), dec!(0.333333), dec!(0.166667)))]
 fn test_get_ratios(#[case] input: Vec<i32>, #[case] output: (Decimal, Decimal, Decimal)) {
     assert_eq!(get_ratios(&input), output);
+}
+
+fn count_pnz(arr: &[i32]) -> (u8, u8, u8) {
+    arr.iter().fold((0, 0, 0), inc_count_pnz)
+}
+
+#[rstest]
+#[case(vec![-4, 3, -9, 0, 4, 1], (3, 2, 1))]
+fn test_count_pnz(#[case] input: Vec<i32>, #[case] output: (u8, u8, u8)) {
+    assert_eq!(count_pnz(&input), output);
+}
+
+fn inc_count_pnz(prev: (u8, u8, u8), val: &i32) -> (u8, u8, u8) {
+    match val.cmp(&0) {
+        Ordering::Greater => (prev.0 + 1, prev.1, prev.2),
+        Ordering::Less => (prev.0, prev.1 + 1, prev.2),
+        Ordering::Equal => (prev.0, prev.1, prev.2 + 1),
+    }
+}
+
+#[rstest]
+#[case(((0, 0, 0), -4), (0, 1, 0))]
+#[case(((0, 1, 0), 3), (1, 1, 0))]
+#[case(((1, 1, 0), -9), (1, 2, 0))]
+#[case(((1, 2, 0), 0), (1, 2, 1))]
+#[case(((1, 2, 1), 4), (2, 2, 1))]
+#[case(((2, 2, 1), 1), (3, 2, 1))]
+fn test_inc_count_pnz(#[case] input: ((u8, u8, u8), i32), #[case] output: (u8, u8, u8)) {
+    let (p, v) = input;
+    assert_eq!(inc_count_pnz(p, &v), output);
 }
